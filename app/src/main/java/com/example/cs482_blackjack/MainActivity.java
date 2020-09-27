c07628a794d3b46701483f855313cc522731ed51
avfallon <31700889+avfallon@users.noreply.github.com>
2020-09-26 20:57:34 -0400

    Outlined Controller, getting an error when I try to add a new card to a card layout

--- app/src/main/java/com/example/cs482_blackjack/MainActivity.java
+++ app/src/main/java/com/example/cs482_blackjack/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.cs482_blackjack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.view.View;
+import android.util.Log;
+import android.widget.GridLayout.LayoutParams;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +24,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Function to get a random card image from the image folder and add it to the appropriate layout
+    public void dealCard(int layoutID ) {
+        GridLayout layout = findViewById(layoutID);
+        ImageView image = new ImageView(this);
+        //This would be a call to the model
+        image.setBackgroundResource(R.drawable.ten_of_hearts);
+        layout.addView(image, 1);
+        Log.w("MA", "Added image");
+    }
+
+    // Function accessed by 'hit' button in view, adds a card to the user's deck
+    public void hit(View myBtn) {
+        dealCard(R.id.userCards);
+    }
+
+
+    // Resets the view to create a new game
+    public void newGame() {
+
+    }
+
+    // Deals two cards to the User and two cards to the Dealer, called at the start of the game
+    public void initialDeal() {
+
+    }
+
+
 }
