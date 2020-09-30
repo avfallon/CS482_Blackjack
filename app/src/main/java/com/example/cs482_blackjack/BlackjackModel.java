5a0ea6cd50d999f74a0db624e9987f579636ce03
avfallon <31700889+avfallon@users.noreply.github.com>
2020-09-30 00:41:46 -0400

    Mostly functional, some math problems in declaring the winner

--- app/src/main/java/com/example/cs482_blackjack/BlackjackModel.java
+++ app/src/main/java/com/example/cs482_blackjack/BlackjackModel.java
@@ -9,48 +9,56 @@ import java.util.Random;
 public class BlackjackModel {
 
     public ArrayList<Integer> userCards = new ArrayList<Integer>();
-    public int[] dealerCards = new int [5];
-    public boolean isDealer = false;
+    public ArrayList<Integer> dealerCards = new ArrayList<Integer>();
     public int userScore;
     public int dealerScore;
+    public int[] cardDeck = fillDeck();
+
+    public static final int NOTOVER = 0;
+    public static final int USERWON = 1;
+    public static final int DEALERWON = 2;
+    public static final int TIE = 3;
 
 
     public BlackjackModel() {
     }
 
 //    Function to get a random card image from the image folder and add it to the appropriate layout
-    public void dealCard(int layoutID ) {
+    // Returns the ID of the dealt card
+    public int getCard(boolean isDealer) {
         // This is the new card image's ID
         int randInt = new Random().nextInt(52);
-        if (!isDealer){
-            userCards
-            userScore = calculateScore(userCards);
-            userCards[](randInt);
+        if (isDealer){
+            dealerCards.add(randInt);
+            dealerScore = calculateScore(dealerCards);
+            Log.w("MA", "dealer score: " + dealerScore);
         }
         else{
-            int dealerScore += cardScore;
-            dealerCards.add(randInt);
+            userCards.add(randInt);
+            Log.w("MA", "userCards:"+userCards);
+            userScore = calculateScore(userCards);
+            Log.w("MA", "user score: " + userScore);
+
         }
-        //GridLayout layout = findViewById(layoutID);
-        ImageView image = findViewById(layoutID);
-        //This would be a call to the model
-        int[] cardDeck = fillDeck();
-        image.setBackgroundResource(cardDeck[randInt]);
-        //layout.addView(image, 1);
-        Log.w("MA", "Added image");
+        return cardDeck[randInt];
     }
 
-    public int calculateScore(int[] cards) {
+    // returns the total score of an entire hand
+    public int calculateScore(ArrayList<Integer> cards) {
+        Log.w("MA", "cards:"+cards);
         int score = 0;
         boolean ace = false;
         for(int i=0;i<cards.size();i++) {
             int cardScore = cardPulledScore(cards.get(i));
+            Log.w("MA", "card score: "+cardScore + cards);
             if(cardScore == 1) {
                 ace = true;
             }
             score += cardScore;
+            Log.w("MA", "myscore1"+score);
         }
         if(ace) {
+            Log.w("MA", "Ace, score:"+score);
             if(score+10 <= 21) {
                 score += 10;
             }
@@ -58,6 +66,7 @@ public class BlackjackModel {
         return score;
     }
 
+    // Returns the score of a given card
     public int cardPulledScore(int randInt){
         if (randInt == 0 || randInt == 1 || randInt == 2 || randInt == 3){
             return 2;
@@ -90,5 +99,65 @@ public class BlackjackModel {
             return 10;
         }
 
+    }
+
+    public int[] fillDeck(){
+
+        return new int[]{R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five, R.drawable.six, R.drawable.seven, R.drawable.eight, R.drawable.nine, R.drawable.ten,
+                R.drawable.eleven, R.drawable.twelve, R.drawable.thirteen, R.drawable.fourteen, R.drawable.fifteen, R.drawable.sixteen, R.drawable.seventeen, R.drawable.eighteen, R.drawable.nineteen, R.drawable.twenty, R.drawable.twenty_one, R.drawable.twenty_two,
+                R.drawable.twenty_three, R.drawable.twenty_four, R.drawable.twenty_five, R.drawable.twenty_six, R.drawable.twenty_seven, R.drawable.twenty_eight, R.drawable.twenty_nine, R.drawable.thirty, R.drawable.thirty_one, R.drawable.thirty_two, R.drawable.thirty_three, R.drawable.thirty_four, R.drawable.thirty_five,
+                R.drawable.thirty_six, R.drawable.thirty_seven, R.drawable.thirty_eight, R.drawable.thirty_nine, R.drawable.forty, R.drawable.forty_one, R.drawable.forty_two, R.drawable.forty_three, R.drawable.forty_four, R.drawable.forty_five, R.drawable.forty_six, R.drawable.forty_seven, R.drawable.forty_eight, R.drawable.forty_nine,
+                R.drawable.fifty, R.drawable.fifty_one, R.drawable.fifty_two};
+
+    }
+
+    public void newGame() {
+        userScore = 0;
+        dealerScore = 0;
+        userCards.clear();
+        dealerCards.clear();
+    }
+
+    // Checks if the game is over, returns the result
+    public int checkGame(boolean isDealer, int dealerHits) {
+        if(checkBlackjack(dealerCards, dealerScore)) {
+            if(checkBlackjack(userCards, userScore)) {
+                return TIE;
+            }
+            return DEALERWON;
+        }
+        else if(checkBlackjack(userCards, userScore)) {
+            return USERWON;
+        }
+        else if(dealerScore == 21) {
+            if(userScore == 21) {
+                return TIE;
+            }
+            return DEALERWON;
+        }
+        else if(userScore == 21) {
+            return USERWON;
+        }
+        else if(userScore > 21) {
+            return DEALERWON;
+        }
+        else if(dealerScore > 21 && userScore < 21) {
+            return USERWON;
+        }
+        else if(isDealer && dealerScore > userScore) {
+            return DEALERWON;
+        }
+        else if(userScore > dealerScore && dealerHits == 2) {
+            return USERWON;
+        }
+
+        return NOTOVER;
+    }
+
+    public boolean checkBlackjack(ArrayList<Integer> cards, int score) {
+        if(cards.size() == 2 && score == 21) {
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/cs482_blackjack/MainActivity.java
+++ app/src/main/java/com/example/cs482_blackjack/MainActivity.java
@@ -22,15 +22,11 @@ public class MainActivity extends AppCompatActivity {
     public int[] userCardViews = {R.id.user0, R.id.user1, R.id.user2, R.id.user3, R.id.user4};
     public int[] dealerCardViews = {R.id.dealer0, R.id.dealer1, R.id.dealer2, R.id.dealer3, R.id.dealer4};
     public int hitCount = 0;
-    public int dealerHitCount = 0;
-    public int userScore = 0;
-    public int dealerScore = 0;
-    public ArrayList<Integer> userCards = new ArrayList<Integer>();
-    public ArrayList<Integer> dealerCards = new ArrayList<Integer>();
     public boolean isDealer = false;
     public boolean gameOver = false;
     public BlackjackModel model;
 
+    // Possible game outcomes
     public static final int NOTOVER = 0;
     public static final int USERWON = 1;
     public static final int DEALERWON = 2;
@@ -40,59 +36,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        buildGuiByCode();
-    }
-
-    public void buildGuiByCode() {
         model = new BlackjackModel();
         setContentView(R.layout.activity_main);
-        initialDeal();
-    }
-
-    public int[] fillDeck(){
-
-        return new int[]{R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five, R.drawable.six, R.drawable.seven, R.drawable.eight, R.drawable.nine, R.drawable.ten,
-                R.drawable.eleven, R.drawable.twelve, R.drawable.thirteen, R.drawable.fourteen, R.drawable.fifteen, R.drawable.sixteen, R.drawable.seventeen, R.drawable.eighteen, R.drawable.nineteen, R.drawable.twenty, R.drawable.twenty_one, R.drawable.twenty_two,
-                R.drawable.twenty_three, R.drawable.twenty_four, R.drawable.twenty_five, R.drawable.twenty_six, R.drawable.twenty_seven, R.drawable.twenty_eight, R.drawable.twenty_nine, R.drawable.thirty, R.drawable.thirty_one, R.drawable.thirty_two, R.drawable.thirty_three, R.drawable.thirty_four, R.drawable.thirty_five,
-                R.drawable.thirty_six, R.drawable.thirty_seven, R.drawable.thirty_eight, R.drawable.thirty_nine, R.drawable.forty, R.drawable.forty_one, R.drawable.forty_two, R.drawable.forty_three, R.drawable.forty_four, R.drawable.forty_five, R.drawable.forty_six, R.drawable.forty_seven, R.drawable.forty_eight, R.drawable.forty_nine,
-                R.drawable.fifty, R.drawable.fifty_one, R.drawable.fifty_two};
-
-    }
+        initialDeal();    }
 
     //Function to get a random card image from the image folder and add it to the appropriate layout
     public void dealCard(int layoutID ) {
-        int randInt = new Random().nextInt(52);
-//        int cardScore = cardPulledScore(randInt);
-        int cardScore = 0;
-        if (!isDealer){
-            userScore += cardScore;
-            userCards.add(randInt);
-        }
-        else{
-            dealerScore += cardScore;
-            dealerCards.add(randInt);
-        }
-        //GridLayout layout = findViewById(layoutID);
-        ImageView image = findViewById(layoutID);
-        //This would be a call to the model
-        int[] cardDeck = fillDeck();
-        image.setBackgroundResource(cardDeck[randInt]);
-        //layout.addView(image, 1);
+        // Gets the ID of a random card image
+        int imageID = model.getCard(isDealer);
+
+        ImageView imageView = findViewById(layoutID);
+        imageView.setBackgroundResource(imageID);
     }
 
     public void hit(View myButton) {
         if(hitCount < 3 && !gameOver) {
-            int image = model.dealCard(userCardViews[hitCount+2]);
-            ImageView view = (ImageView) findViewById(userCardViews[hitCount+2]);
-            image.setBackgroundResource
-
+            dealCard(userCardViews[hitCount+2]);
             hitCount++;
 
             // Checks if the game is over
-            int result = checkGame();
+            int result = model.checkGame(isDealer, 0);
             Log.w("MA", "result: "+result);
             if(result > 0)  {
                 endMessage(result);
+                gameOver = true;
             }
         }
     }
@@ -109,61 +76,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public int checkGame() {
-        gameOver = true;
-        if(checkBlackjack(dealerCards, dealerScore)) {
-            if(checkBlackjack(userCards, userScore)) {
-                return TIE;
-            }
-            return DEALERWON;
-        }
-        else if(checkBlackjack(userCards, userScore)) {
-            return USERWON;
-        }
-        else if(dealerScore == 21) {
-            if(userScore == 21) {
-                return TIE;
-            }
-            return DEALERWON;
-        }
-        else if(userScore == 21) {
-            return USERWON;
-        }
-        else if(userScore > 21) {
-            return DEALERWON;
-        }
-        else if(dealerScore > 21 && userScore < 21) {
-            return USERWON;
-        }
-        else if(isDealer && dealerScore > userScore) {
-            return DEALERWON;
-        }
-        else if(dealerHitCount == 3 && userScore > dealerScore) {
-            return USERWON;
-        }
-        gameOver = false;
-        return NOTOVER;
-
-    }
-
-    public boolean checkBlackjack(ArrayList<Integer> cards, int score) {
-        if(cards.size() == 2 && score == 21) {
-            return true;
-        }
-        return false;
-    }
-
     // Resets the view to create a new game
     public void newGame(View myBtn) {
         hitCount = 0;
-        userScore = 0;
-        dealerScore = 0;
-        userCards.clear();
         gameOver = false;
+        findViewById(R.id.hitBtn).setClickable(true);
+        ((TextView)findViewById(R.id.user_msg)).setText("Hit or Stop?");
 
         // Removes all images from both hands
         clearDeck(userCardViews);
         clearDeck(dealerCardViews);
+        model.newGame();
 
         initialDeal();
     }
@@ -182,47 +105,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initialDeal() {
-        userScore = dealTwo(R.id.user0, R.id.user1, userCards);
-        dealerScore = dealTwo(R.id.dealer0, R.id.dealer1, dealerCards);
-    }
+        dealCard(R.id.user0);
+        dealCard(R.id.user1);
 
-    public int dealTwo(int cardID_1, int cardID_2, ArrayList<Integer> cardList) {
-        ImageView cardOne = findViewById(cardID_1);
-        ImageView cardTwo = findViewById(cardID_2);
-
-        // Gets the ID of the card images
-        int randInt1 = new Random().nextInt(52);
-        int randInt2 = new Random().nextInt(52);
-
-        //adds the images to the empty views
-        cardList.add(randInt1);
-        cardList.add(randInt2);
-
-        int score = 0;
-        int firstScore = cardPulledScore(randInt1);
-        score += firstScore;
-        int secondScore = cardPulledScore(randInt2);
-        score += secondScore;
-
-        int[] cardDeck = fillDeck();
-        cardOne.setBackgroundResource(cardDeck[randInt1]);
-        cardTwo.setBackgroundResource(cardDeck[randInt2]);
-
-        Log.w("MA", "inside: "+cardList);
-        return score;
+        isDealer = true;
+        dealCard(R.id.dealer0);
+        dealCard(R.id.dealer1);
+        isDealer = false;
     }
 
     public void dealersTurn(){
-        if (userScore > 21){
-            return;
-        }
-        else if (dealerScore == 21){
-            if (userScore == 21){
-                System.out.println("Tie");
+        int dHitCount = 0;
+        Log.w("MA", "dealer:");
+        while(dHitCount < 3 && !gameOver) {
+            dealCard(dealerCardViews[hitCount+2]);
+            dHitCount++;
+
+            // Checks if the game is over
+            int result = model.checkGame(isDealer, dHitCount);
+            if(result > 0)  {
+                endMessage(result);
+                gameOver = true;
             }
-        }
-        while (dealerScore < 17 || dealerScore <= userScore){
-            findViewById(R.id.hitBtn).performClick();
+
+            dHitCount++;
+
         }
     }
 }
